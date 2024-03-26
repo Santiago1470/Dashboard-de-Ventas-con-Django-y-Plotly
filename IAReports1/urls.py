699adfa5596547d9eacd82c9d648307051a3847d
Santiago1470 <st147086@gmail.com>
2024-03-25 20:00:54 -0500

    Se agregaron archivos del repositorio base del profesor

---
+++ IAReports1/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from report import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('dash/', views.index, name="dash"),
+]
---
+++ report/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import ConexionDB
+
+
+
+class ConexionDBAdmin(admin.ModelAdmin):
+    list_display = ("name", "host", "user")
+    search_fields = ("name", "host")
+
+
+admin.site.register(ConexionDB, ConexionDBAdmin)
+
